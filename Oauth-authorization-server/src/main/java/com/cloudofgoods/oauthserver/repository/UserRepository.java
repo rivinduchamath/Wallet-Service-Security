60835bfdcd2ff3fe121c5d233009331383e826cf
rivindu2 <rivindu@incubatelabs.com>
2022-09-19 19:58:33 +0530

    Working  1.0'

---
+++ Oauth-authorization-server/src/main/java/com/cloudofgoods/oauthserver/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cloudofgoods.oauthserver.repository;
+
+
+import com.cloudofgoods.oauthserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
---
+++ Oauth-authorization-server/src/test/java/com/cloudofgoods/oauthserver/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudofgoods.oauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
